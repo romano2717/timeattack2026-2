9bb867024871d01f0223958604e78b30c91fa73f
romano2717 <romano2717@gmail.com>
2026-07-31 16:09:50 +0800

    Update registration.js

--- registration.js
+++ registration.js
@@ -1,5 +1,5 @@
 const GOOGLE_SCRIPT_URL =
-  "PASTE_YOUR_GOOGLE_APPS_SCRIPT_WEB_APP_URL_HERE";
+  "https://script.google.com/macros/s/AKfycbxdsuMwyvYnad4VKe3SwgWF02br9ZFJTTkOf4yHvKil4GmsJu9y7wuxaTY5m_R3fJVJ/exec";
 
 // Change this one value when the registration period changes:
 // "Early", "Late", or "Walk-in"
